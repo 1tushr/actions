f8660c46d7624d96120d869fe7aef03840b0c610
Tushar Verma <tuhsar2022@gmail.com>
2023-05-07 01:51:26 +0530

    updated login signup

--- account/login.js
+++ account/login.js
@@ -1,34 +1,20 @@
-let details = document.querySelectorAll('input');
-let login = JSON.parse(localStorage.getItem('login')) || [];
 
-for (let i = 0; i < details.length; i++) {
-  details[i].value = login[i]|| "";
-}
+function login() {
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    
+    // Get the user's information from local storage
+    const user = JSON.parse(localStorage.getItem(email));
+    
+    if (user && user.password === password) {
+      alert('Login successful!');
 
-function inputs() {
-  for (let i = 0; i < details.length; i++) {
-    login[i] = details[i].value;
-  }
-  console.log(login);
-  localStorage.setItem('login', JSON.stringify(login));
-
-  // checking the login details
-  let signup = JSON.parse(localStorage.getItem('signups')) || [];
-  if (login[0] === signup[2] && login[1] === signup[3]) {
-    console.log('Login successful');
-  } else {
-    console.log('Login failed');
+     
+      // Redirect to the dashboard or home page
+    } else {
+      alert('Invalid email or password. Please try again.');
+    }
   }
   
-console.log('Compared signup values:', signup);
-console.log(login[0] )
-console.log(login[1] )
-console.log(signup[2])
-console.log(signup[3]);
-}
-
-let btn = document.getElementById('btn');
-btn.addEventListener('click', inputs);
-
-console.log('Compared login values:', login);
-
+  btn.addEventListener('click', login);
+  
--- account/signup.js
+++ account/signup.js
@@ -1,25 +1,49 @@
-const inputs = document.querySelectorAll('input');
 
-const signup=JSON.parse(localStorage.getItem('signups'))||[];
 
-for(let i=0;i<inputs.length;i++){
-     inputs[i].value=signup[i]||"";
+function saveSignup(){
+     let firstName,lastName,email,password,confirmPassword;
+     firstName=document.getElementById('first-name').value;
+     lastName=document.getElementById('last-name').value;
+     email=document.getElementById('email').value;
+     password=document.getElementById('password').value;
+     confirmPassword=document.getElementById('confirm-password').value;
+
+     // Check if email already exists in local storage
+     const existingUser = localStorage.getItem(email);
+     if (existingUser) {
+         // If email exists, alert user and ask them to update their info
+         if (confirm('This email already exists. Do you want to update your information?')) {
+             localStorage.setItem('firstName', firstName);
+             localStorage.setItem('lastName', lastName);
+             localStorage.setItem('email', email);
+             localStorage.setItem('password', password);
+             localStorage.setItem('confirmPassword', confirmPassword);
+             alert('Information updated successfully!');
+         }
+     } else {
+         // If email does not exist, save the new information to local storage
+         localStorage.setItem(email, JSON.stringify({
+             firstName,
+             lastName,
+             password,
+             confirmPassword
+         }));
+         alert('Sign up successful!');
+         window.location.href="login.html";
+     }
 }
 
+btn.addEventListener('click', saveSignup);
 
 
 
-function submit(){
-     for (let i = 0; i < inputs.length; i++) {
-          signup[i]=inputs[i].value;
-          
-        }
-        console.log(signup);
-        
-localStorage.setItem('signups',JSON.stringify(signup));
-}
 
 
-btn.addEventListener('click',submit)
+
+
+
+
+
+
 
 
