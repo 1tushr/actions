f7f6615ced295a1efec50d3992423e1669cb66c2
Tushar Verma <tuhsar2022@gmail.com>
2023-05-06 21:34:17 +0530

    login updated

--- account/login.js
+++ account/login.js
@@ -1,12 +1,34 @@
-let details=document.querySelectorAll('input');
+let details = document.querySelectorAll('input');
+let login = JSON.parse(localStorage.getItem('login')) || [];
 
-let login=[];
-
-function inputs(){
-    for(let i=0;i<details.length;i++){
-        login.push(details[i].value);
-    }
-    console.log(login);
+for (let i = 0; i < details.length; i++) {
+  details[i].value = login[i]|| "";
 }
 
-btn.addEventListener('click',inputs);
+function inputs() {
+  for (let i = 0; i < details.length; i++) {
+    login[i] = details[i].value;
+  }
+  console.log(login);
+  localStorage.setItem('login', JSON.stringify(login));
+
+  // checking the login details
+  let signup = JSON.parse(localStorage.getItem('signups')) || [];
+  if (login[0] === signup[2] && login[1] === signup[3]) {
+    console.log('Login successful');
+  } else {
+    console.log('Login failed');
+  }
+  
+console.log('Compared signup values:', signup);
+console.log(login[0] )
+console.log(login[1] )
+console.log(signup[2])
+console.log(signup[3]);
+}
+
+let btn = document.getElementById('btn');
+btn.addEventListener('click', inputs);
+
+console.log('Compared login values:', login);
+
--- f3-project-main/shop/script.js
+++ f3-project-main/shop/script.js
@@ -1,10 +1,31 @@
-const produtc = {
-  id: 1,
-  title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-  price: 109.95,
-  description:
-    "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-  category: "men's clothing",
-  image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-  rating: { rate: 3.9, count: 120 },
-};
+// const produtc = {
+//   id: 1,
+//   title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+//   price: 109.95,
+//   description:
+//     "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+//   category: "men's clothing",
+//   image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+//   rating: { rate: 3.9, count: 120 },
+// };
+let data=[];
+fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(jsonData => {
+    data = jsonData;
+    console.log(data);
+
+//     // Set the image from men category in mensImage
+//     const menCategory = data.filter(item => item.category === 'men\'s clothing')[3];
+//     let mensImage=document.getElementById('mens-image');
+//     mensImage.src = menCategory.image;
+ });
+
+
+
+
+
+
+  
+
+
